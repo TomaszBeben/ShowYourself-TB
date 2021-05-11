5906a3d49c3353aab1ad0c449d03fd945c4f1893
TomaszBeben <tbeben@st.swps.edu.pl>
2021-05-11 12:56:33 +0200

    third.3

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,9 @@
 import firebase from 'firebase'
 import 'firebase/auth'
-import { firebaseApi } from '../ignore/indywidualFirebaseApi'
+import { firebaseApi } from './ignore/firebaseAPI'
 
 const firebaseApp = firebase.initializeApp(
   firebaseApi
-  // apiKey: "AIzaSyBO9uASvkUUEg4fCFLZ2DgF5MY34cP9yHk",
-  // authDomain: "showyourself-tb.firebaseapp.com",
-  // projectId: "showyourself-tb",
-  // storageBucket: "showyourself-tb.appspot.com",
-  // messagingSenderId: "437706410699",
-  // appId: "1:437706410699:web:a0a60d06e7c957e3ee294e"
 )
 
 export const auth = firebaseApp.auth()
