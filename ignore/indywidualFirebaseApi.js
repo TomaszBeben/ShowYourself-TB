c840cdd472320931ca7345dfb847a453bbf04772
TomaszBeben <tbeben@st.swps.edu.pl>
2021-05-11 12:53:25 +0200

    third

--- ignore/indywidualFirebaseApi.js
+++
@@ -1,8 +0,0 @@
-export const firebaseApi = {
-        apiKey: "AIzaSyBO9uASvkUUEg4fCFLZ2DgF5MY34cP9yHk",
-        authDomain: "showyourself-tb.firebaseapp.com",
-        projectId: "showyourself-tb",
-        storageBucket: "showyourself-tb.appspot.com",
-        messagingSenderId: "437706410699",
-        appId: "1:437706410699:web:a0a60d06e7c957e3ee294e"
-      }
