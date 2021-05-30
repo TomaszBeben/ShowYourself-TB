b79e8d851d8c329888431696268f673714f94d7a
TomaszBeben <tbeben@st.swps.edu.pl>
2021-05-30 17:07:26 +0200

    footer implementation

--- src/components/creator/footer/Footer.js
+++ src/components/creator/footer/Footer.js
@@ -3,7 +3,22 @@ import React from 'react'
 const Footer = () => {
     return (
         <div>
-            <div className='main-page__footer-container'></div>
+            <div className='main-page__footer--container'>
+                <div className='main-page__footer__container--left'>
+                        <a href='https://tbebenportfolio.web.app/#/'>
+                            <div className='main-page__footer--elem footer-portfolio'></div>
+                        </a>
+                        <a href='https://github.com/TomaszBeben'>
+                            <div className='main-page__footer--elem footer-github'></div>
+                        </a>
+                        <a href='https://www.linkedin.com/in/tomasz-b%C4%99ben-1944021a2/'>
+                            <div className='main-page__footer--elem footer-linkedin'></div>
+                        </a>
+                </div>
+                <div className='main-page__footer__container--right'>
+                    <div className='main-page__footer--elem'></div>
+                </div>
+            </div>
         </div>
     )
 }
