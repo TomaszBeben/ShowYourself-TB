b0ff1edb1eee81bd5e9ae9a20bafea87f7839bce
TomaszBeben <tbeben@st.swps.edu.pl>
2021-08-31 17:53:29 +0200

    test

--- src/components/creator/cvCreator/creatorForms/Consents.js
+++ src/components/creator/cvCreator/creatorForms/Consents.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import { TextField } from '@material-ui/core'
-import  useStyles  from '../styles'
-
 
 const Consent = ({ postData, setPostData }) => {
-    const classes = useStyles()
     return (
         <>
             <TextField multiline fullWidth type="text" label='Consent' value={postData.consent} onChange={(e) => setPostData({ ...postData, consent: e.target.value })} />
